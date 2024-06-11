5b9fda0421ec4166061e4d9181d199f1e948bf01
dantegarden <761043617@qq.com>
2024-06-11 20:17:19 +0800

    add: cors

---
+++ app/internal/middleware/cors_middleware.go
@@ -0,0 +1,22 @@
+package middleware
+
+import "net/http"
+
+type CorsMiddleware struct {
+}
+
+func NewCorsMiddleware() *CorsMiddleware {
+	return &CorsMiddleware{}
+}
+
+func (m *CorsMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//解决跨域
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		// Passthrough to next handler if need
+		next(w, r)
+	}
+}
